2a2cfd2113c15f1473eaf4f54625710d324cf8ba
Arthur Cesar F S Nogueira <arthur.nogueira@fitbank.com.br>
2022-05-26 10:11:34 -0300

    Adding steps in Program.cs file.

---
+++ Projeto1/Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿namespace Projeto1.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
